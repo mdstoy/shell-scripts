ef3cf16dcf1b9c064c92a9fbd487a10291b2853a
mdstoy <toyofyou.me@gmail.com>
2018-04-03 19:58:20 +0900

    add back and direct index

--- prompt/next.sh
+++ prompt/next.sh
@@ -8,21 +8,25 @@ function execute(){
 
 TARGET_DIR=$1
 
-echo ${TARGET_DIR}
-
 TARGET_FILES=($(find ${TARGET_DIR} -maxdepth 1 -type f | sort))
 
-FILE_INDEX=0
+set +u
+START_INDEX=$2
+FILE_INDEX=${START_INDEX:=0}
+set -u
             
 execute ${TARGET_FILES[${FILE_INDEX}]}
 
 while :
 do
-    echo "? (n/r/q): "
+    echo "? (n/b/r/q/NUMBER): "
     read value
     case ${value} in
         [nN] )
             FILE_INDEX=$((FILE_INDEX + 1))
+            execute ${TARGET_FILES[${FILE_INDEX}]} ;;
+        [bB] )
+            FILE_INDEX=$((FILE_INDEX - 1))
             execute ${TARGET_FILES[${FILE_INDEX}]} ;;
         [rR] )
             execute ${TARGET_FILES[${FILE_INDEX}]} ;;
@@ -30,7 +34,15 @@ do
             echo "bye."
             break ;;
         * )
-            echo "please enter n, r or q" ;;
+            expr ${value} + 1 > /dev/null 2>&1
+            if [ $? -lt 2 ]
+            then
+                FILE_INDEX=${value}
+                execute ${TARGET_FILES[${FILE_INDEX}]}
+            else
+                echo "please enter n, b, r, q, or NUMBER"
+            fi
+            ;;
     esac
 done
 
